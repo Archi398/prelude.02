bc73b4adb09ec83adb1b1c4f92cca84fd117c850
Archi <archisabatier@gmail.com>
2021-09-27 09:22:30 +0200

    Update e01.js

--- e01.js
+++ e01.js
@@ -1,1 +1,12 @@
-salut
+function first(array, n = 1) {
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === n) {
+            return i + 1
+        }
+    }
+
+}
+
+
+console.log(first([5, 4, 3, 2, 1], 4));
