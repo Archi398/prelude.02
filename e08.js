39080055daf89b44e9647755b18389acaf5f0d64
Archi398 <archisabatier@gmail.com>
2021-09-30 09:22:26 +0200

    Create e08.js

---
+++ e08.js
@@ -0,0 +1,13 @@
+function union() {
+    let tab = [];
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j <= arguments.length; j++) {
+            if(!tab.includes(arguments[i][j]) && (arguments[0].includes(arguments[i][j])) && (typeof(arguments[i][j]) != 'undefined')){
+                tab.push(arguments[i][j])
+            }
+        }
+    }
+    return tab
+}
+
+console.log(union([1, 2, 3], [3, 101, 2, 1, 10], [2, 1, 4, 3]));
