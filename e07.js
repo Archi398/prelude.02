0e29fcbee78117b4eb9b65aca54c437b1a9a58bb
Archi398 <archisabatier@gmail.com>
2021-09-30 08:36:00 +0200

    Create e07.js

---
+++ e07.js
@@ -0,0 +1,14 @@
+function union(array) {
+    let tab = [];
+    let found = false;
+    for (let i = 0; i < arguments.length; i++) {
+        for (let j = 0; j <= arguments.length; j++) {
+            if(!tab.includes(arguments[i][j]) && (typeof(arguments[i][j]) != 'undefined')){
+                tab.push(arguments[i][j])
+            }
+        }    
+    }
+    return tab;
+}
+
+console.log(union([1, 2, 3], [101, 2, 1, 10], [2, 1]));
